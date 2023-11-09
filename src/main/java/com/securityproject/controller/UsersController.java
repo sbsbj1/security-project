c074f3904eac035aab508fd5f004dc4e941f74d6
sbsbj1 <lucasacevedo095@gmail.com>
2023-11-09 19:09:36 -0300

    second version

---
+++ src/main/java/com/securityproject/controller/UsersController.java
@@ -0,0 +1,25 @@
+package com.securityproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsersController {
+
+    @GetMapping("/register")
+    public String getRegisterPage(){
+        return "register_page";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(){
+        return "login_page";
+    }
+
+    @GetMapping("/")
+    public String getIndexPage() {
+        return "index";
+    }
+
+
+}
--- src/main/java/com/securityproject/repository/UsersRepository.java
+++ src/main/java/com/securityproject/repository/UsersRepository.java
@@ -4,9 +4,11 @@ package com.securityproject.repository;
 import com.securityproject.model.UsersModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository  extends JpaRepository<UsersModel, Integer> {
 
-    Optional<UsersModel> findByLoginAndPassword(String login, String password)
+    Optional<UsersModel> findByLoginAndPassword(String login, String password);
 
 
 }
